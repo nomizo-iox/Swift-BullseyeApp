9e2016469e56d5a336da67f78ea08633aeab9f5f
nomizo-iox <nomizo.iox@gmail.com>
2019-10-28 23:26:33 -0500

    2nd commit.

---
+++ bullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bullsEye
+//
+//  Created by Samuel F. Ademola on 10/28/19.
+//  Copyright © 2019 Nomizo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
